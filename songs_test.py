5957adf65fc0b6911815593b5d143f3d0a2da862
Bruno-Pierre Jobin <bpjobin@gmail.com>
2019-02-26 20:56:13 -0500

    playing with tunes

--- songs_test.py
+++ songs_test.py
@@ -5,21 +5,24 @@ from rtttl import RTTTL
 import songs
 
 piezo_pin = PWM(Pin(5))
-
+volume = 20
 
 def play_tone(freq, msec):
 	""""""
 	print('freq = {:6.1f} msec = {:6.1f}'.format(freq, msec))
 	if freq > 0:
 		piezo_pin.freq(int(freq))
-		piezo_pin.duty(50)
+		piezo_pin.duty(volume)
 	time.sleep_ms(int(msec * 0.9))
 	piezo_pin.duty(0)
 	time.sleep_ms(int(msec * 0.1))
 
 
-def play_song(s):
+def play_song(s, duration):
 	""""""
+	note = 0
 	tune = RTTTL(songs.find(s))
 	for freq, msec in tune.notes():
-		play_tone(freq, msec)
+		if note < duration:
+			play_tone(freq, msec)
+		note += 1
