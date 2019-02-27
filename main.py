8337177eebeae437d885e8ab7d2796d3b1d4bd1b
Bruno-Pierre Jobin <bpjobin@gmail.com>
2019-02-26 21:17:38 -0500

    Revert songs
    Removed songs functions. Only dispensing food now.

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from machine import Pin
 from machine import PWM
 from machine import unique_id
 from umqtt.robust import MQTTClient
-from rtttl import RTTTL
-import songs
 
 from credentials import MQTT_SERVER
 from credentials import MQTT_USERNAME
@@ -16,8 +14,6 @@ CLIENT_ID = "cat_feeder"
 
 global closed_position
 closed_position = 75
-
-piezo_pin = PWM(Pin(5))
 
 servo_buzz = PWM(
 	Pin(14),
@@ -49,7 +45,7 @@ def mqtt_connect():
 		)
 
 	client.set_callback(on_message)
-	print("Connecting MQTT as clean session")
+	print("Connecting MQTT")
 	client.set_last_will(
 		"cat_feeder/status",
 		"Offline",
@@ -77,23 +73,17 @@ def on_message(topic, message):
 	""""""
 	print(topic, message)
 
-	topic = message.strip().decode('utf-8')
-	msg = topic.split(":")[0]
-	song = topic.split(":")[-1]
+	msg = message.strip().decode('utf-8')
 
 	if msg == "feed":
-	 	client.publish("cat/last_fed", "Feeding...")
-	 	done_payload = 'Done feeding!'
+		client.publish("cat/last_fed", "Feeding...")
+		done_payload = 'Done feeding!'
 
-	 	try:
-			if song != "None":
-				play_song(song)
-				time.sleep(1)
+		try:
 			open_and_close()
 		except:
 			done_payload = 'Error. Bad message format. Payload was: %s' % msg
 		
-
 		client.publish(
 		"cat_feeder/done_feeding",
 		done_payload
@@ -104,30 +94,13 @@ def on_message(topic, message):
 
 def open_and_close():
 	""""""
-	servo_buzz.duty(47)
-	time.sleep(0.21)
+	servo_buzz.duty(47) # petite croquette --> 47
+	time.sleep(0.25) # petite croquette --> 0.21
 	servo_buzz.duty(closed_position)
 
 	servo_tuxedo.duty(45)
 	time.sleep(0.365)
 	servo_tuxedo.duty(closed_position)
 
-
-def play_tone(freq, msec):
-	""""""
-    print('freq = {:6.1f} msec = {:6.1f}'.format(freq, msec))
-    if freq > 0:
-        buz_tim.freq(int(freq))
-        buz_tim.duty(50)
-    time.sleep_ms(int(msec * 0.9))
-    buz_tim.duty(0)
-    time.sleep_ms(int(msec * 0.1))
-
-
-def play_song(s):
-	""""""
-    tune = RTTTL(songs.find(s))
-    for freq, msec in tune.notes():
-        play_tone(freq, msec)
 
 mqtt_connect()
